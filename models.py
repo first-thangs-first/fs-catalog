ff9a918f0bc6a64270797468a7cbfebd20d2088a
vagrant <vagrant@vagrant.vm>
2017-12-08 04:11:47 +0000

    added cascade option to catalogitem with backref to category, so that items with category deleted will get deleted as well

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import backref
 
 import random, string
 from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
@@ -36,9 +37,7 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    # user_id = Column(Integer, ForeignKey('user.id'))
-    # user = relationship(User)
-
+    
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
@@ -54,10 +53,10 @@ class CatalogItem(Base):
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, cascade="all, delete-orphan", single_parent=True)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category, backref=backref("category", cascade="all,delete"))
 
     @property
     def serialize(self):
